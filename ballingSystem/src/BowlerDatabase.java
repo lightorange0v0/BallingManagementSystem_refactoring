3f2fe6ab6ecd0518f2942cf9e10d021bb2d01f6f
lightorange0v0 <94609651+lightorange0v0@users.noreply.github.com>
2023-05-31 14:59:02 +0900

    Delete ballingSystem directory

--- ballingSystem/src/BowlerDatabase.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-import java.util.List;
-
-public interface BowlerDatabase {
-	Bowler getBowlerInfo(String nickName) throws IOException;
-    void putBowlerInfo(String nickName, String fullName, String email) throws IOException;
-    List<String> getBowlers() throws IOException;
-}
