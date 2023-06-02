fb1e5a7a2bed293e918eba99eeb3420a1a3460ad
minsu <kimms5617@naver.com>
2023-06-02 17:22:52 +0900

    [Refactoring] Strategy pattern of getScore() in Lane.java

--- FoulScoringStrategy.java
+++ FoulScoringStrategy.java
@@ -3,4 +3,9 @@ class FoulScoringStrategy implements ScoringStrategy {
     public String getScoreSymbol(int[] bowlerScores, int ballIndex) {
         return (bowlerScores[ballIndex] == -2) ? "F" : null;
     }
+
+    @Override
+    public void computeScore(int[] curScore, int[][] cumulScores, int bowlIndex, int current, int i) {
+        // foul은 점수 계산에 영향이 없음
+    }
 }
--- SpareScoringStrategy.java
+++ SpareScoringStrategy.java
@@ -3,4 +3,15 @@ class SpareScoringStrategy implements ScoringStrategy {
     public String getScoreSymbol(int[] bowlerScores, int ballIndex) {
         return (ballIndex > 0 && bowlerScores[ballIndex] + bowlerScores[ballIndex - 1] == 10 && ballIndex % 2 == 1) ? "/" : null;
     }
+
+    @Override
+    public void computeScore(int[] curScore, int[][] cumulScores, int bowlIndex, int current, int i) {
+        //        This ball was a the second of a spare.
+        //Also, we're not on the current ball.
+        //Add the next ball to the ith one in cumul.
+        cumulScores[bowlIndex][i/2] += curScore[i+1] + curScore[i];
+        if (i > 1) {
+            //cumulScores[bowlIndex][i/2] += cumulScores[bowlIndex][i/2 -1];
+        }
+    }
 }
